3467c49433500146a027c92f162886a34853540c
Alexander Ramos Jardim <alexander@volanty.com>
2020-04-24 10:17:57 -0300

    docker build

--- df_functions.py
+++ df_functions.py
@@ -1,19 +1,6 @@
 import numpy as np
 import pandas as pd
 
-def generateCarEntries(manufacturer, model):
-    date_range = pd.date_range('2019-10-01', periods=90, freq='D')
-    minimum_margin = np.random.uniform(low=0.13, high=0.16, size=len(date_range))
-    average_margin = np.random.uniform(low=0.165, high=0.185, size=len(date_range))
-    maximum_margin = np.random.uniform(low=0.19, high=0.23, size=len(date_range))
-    df = pd.DataFrame({'date': date_range,
-        'min_margin': minimum_margin,
-        'avg_margin': average_margin,
-        'max_margin': maximum_margin
-    })
-    df.set_index('date', inplace=True)
-    return df
-
 def getMarginOverTime(df):
     return df.groupby(['Date']).agg(['min', 'mean', 'max'])['Margin']
 
